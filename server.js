fb7e7f0ff30afb9d39831c0d21864a01b2c1a0ed
Utkarsh Tyagi <utkarsh17ife@gmail.com>
2016-05-19 09:54:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public')); 
+app.get("/", function(req, res){
+    res.sendFile( __dirname + "/" + "index.html");
+});
+
+var io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
+//console.log("Listening on port " + port);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('chat_message', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
